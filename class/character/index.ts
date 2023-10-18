664c1d5aa7fbf369ff51b7a60a0ed4350e6bae0b
nova_44056 <senguy44056@gmail.com>
2023-10-18 12:26:46 -0500

    add character properties

--- class/character/index.ts
+++ class/character/index.ts
@@ -4,7 +4,13 @@ export class Character {
   constructor(
     protected _name: string,
     protected _age: number,
-    protected _gender: GENDER
+    protected _gender: GENDER,
+    // TODO: expand occupation into class
+    protected _occupation: string,
+    // TODO: expand physical health and mental health into health class
+    protected _physical_health: number = 100,
+    protected _mental_health: number = 100,
+    protected _money: number = 0
   ) {}
 
   name() {
@@ -17,5 +23,21 @@ export class Character {
 
   gender() {
     return this._gender;
+  }
+
+  occupation() {
+    return this._occupation;
+  }
+
+  physical_health() {
+    return this._physical_health;
+  }
+
+  mental_health() {
+    return this._mental_health;
+  }
+
+  money() {
+    return this._money;
   }
 }
