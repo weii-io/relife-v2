04cd0248a9c9fffec4a5d21b2c6ea6b9b2ab50ce
nova_44056 <senguy44056@gmail.com>
2023-10-18 12:58:43 -0500

    initialize game storage in game class

--- class/game/index.ts
+++ class/game/index.ts
@@ -1,11 +1,24 @@
 import { CharacterEngine } from "./engines/character";
 import { PlayerEngine } from "./engines/player";
 import { WorldEngine } from "./engines/world";
+import { GameStorage } from "./storage";
 
 export class Game {
+  protected _version: number = 2;
+  protected _name = "relife";
+  protected _storage: GameStorage = new GameStorage(this.name, this.version);
+
   constructor(
     protected _character_engine: CharacterEngine,
     protected _player_engine: PlayerEngine,
     protected _world_engine: WorldEngine
   ) {}
+
+  get version() {
+    return this._version;
+  }
+
+  get name() {
+    return this._name;
+  }
 }
--- class/game/storage/index.ts
+++ class/game/storage/index.ts
@@ -1,4 +1,4 @@
-export class Storage {
+export class GameStorage {
   private db!: IDBDatabase;
 
   constructor(private dbName: string, private version: number) {
